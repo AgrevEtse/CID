d16a1d3e03874b47519ad8970432081bad72976c
Diego Vazquez <defren3211@gmail.com>
2024-05-16 17:14:27 -0600

    Agregando tipado al Hands-on 4

--- Hands-on 4/KNNClassifier.py
+++ Hands-on 4/KNNClassifier.py
@@ -1,33 +1,33 @@
 from DiscreteMaths import DiscreteMaths
 
 class KNNClassifier:
-    def __init__(self, k = 5, metric = 'euclidean'):
-        self.k = k
-        self.metric = metric
+    def __init__(self, k = 5, metric = 'euclidean') -> None:
+        self.k: int = k
+        self.metric: str = metric
 
     # Training dara
-    def fit(self, X_train, y_train):
-        self.X_train = X_train
-        self.y_train = y_train
-        self.dm = DiscreteMaths(len(X_train[0]))
+    def fit(self, X_train, y_train) -> None:
+        self.X_train: list[float] = X_train
+        self.y_train: list[float] = y_train
+        self.dm: DiscreteMaths = DiscreteMaths(len(X_train[0]))
 
     # Make predictions based on training data
-    def predict(self, X_test):
-        distances = self._compute_distances(X_test)
-        nearest_neighbors = self._get_nearest_neighbors(distances)
-        predictions = self._majority_vote(nearest_neighbors)
+    def predict(self, X_test) -> list[int]:
+        distances: list[float] = self._compute_distances(X_test)
+        nearest_neighbors: list[float] = self._get_nearest_neighbors(distances)
+        predictions: list[int] = self._majority_vote(nearest_neighbors)
 
         return predictions
 
     # Compute distances between test and training data
-    def _compute_distances(self, X_test):
-        distances = []
+    def _compute_distances(self, X_test: list[float]) -> list[float]:
+        distances: list[float] = []
 
         for x_test in X_test:
-            distances_i = []
+            distances_i: list[float] = []
 
             for x_train in self.X_train:
-                distance = self._calculate_distance(x_test, x_train)
+                distance: float = self._calculate_distance(x_test, x_train)
                 distances_i.append(distance)
 
             distances.append(distances_i)
@@ -35,7 +35,7 @@ class KNNClassifier:
         return distances
 
     # Calculate distance between two points
-    def _calculate_distance(self, x1, x2):
+    def _calculate_distance(self, x1: list[float], x2: list[float]) -> float:
         if self.metric == 'euclidean':
             return self.dm.euclidean_distance(x1, x2)
 
@@ -43,15 +43,15 @@ class KNNClassifier:
             return self.dm.manhattan_distance(x1, x2)
 
     # Get nearest neighbors based on distances
-    def _get_nearest_neighbors(self, distances):
-        nearest_neighbors = []
+    def _get_nearest_neighbors(self, distances: list[float]) -> list[float]:
+        nearest_neighbors: list[float] = []
 
         for distances_i in distances:
-            indices = []
+            indexes: list[float] = []
 
             for i in range(len(distances_i)):
-                min_distance = distances_i[0]
-                min_index = 0
+                min_distance: float = distances_i[0]
+                min_index: int = 0
 
                 # Find the minimum distance
                 for j in range(1, len(distances_i)):
@@ -59,15 +59,15 @@ class KNNClassifier:
                         min_distance = distances_i[j]
                         min_index = j
 
-                indices.append(min_index)
+                indexes.append(min_index)
 
             # Get the k nearest neighbors
-            if len(indices) > self.k:
-                indices = indices[:self.k]
+            if len(indexes) > self.k:
+                indexes = indexes[:self.k]
 
-            nearest_neighbors_i = []
+            nearest_neighbors_i: list[int] = []
 
-            for i in indices:
+            for i in indexes:
                 nearest_neighbors_i.append(self.y_train[i])
 
             nearest_neighbors.append(nearest_neighbors_i)
@@ -75,8 +75,8 @@ class KNNClassifier:
         return nearest_neighbors
 
     # Get the majority vote
-    def _majority_vote(self, nearest_neighbors):
-        predictions = []
+    def _majority_vote(self, nearest_neighbors: list[float]) -> list[int]:
+        predictions: list[int] = []
 
         # Get the majority vote for each nearest neighbor
         for nearest_neighbors_i in nearest_neighbors:
@@ -88,8 +88,8 @@ class KNNClassifier:
 
                 vote_counts[label] += 1
 
-            max_votes = 0
-            max_label = None
+            max_votes: int = 0
+            max_label: int = None
 
             # Get the label with the most votes
             for label, votes in vote_counts.items():
